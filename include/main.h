e44c03474ef6761216a30fd0f07986aff101f469
Christopher Canaday <chrislcanaday@gmail.com>
2022-09-23 12:53:24 -0400

    first iteration of header

---
+++ include/main.h
@@ -0,0 +1,14 @@
+#ifndef SPIROGRAPH_INCLUDE_MAIN_H
+#define SPIROGRAPH_INCLUDE_MAIN_H
+
+struct Spirograph{
+    double theta; // angle
+    double r_i; // inner circle radius
+    double r_o; // outer circle radius
+    double c_i_dist; // distance from edge of inside circle
+    double x;
+    double y;
+};
+
+
+#endif
