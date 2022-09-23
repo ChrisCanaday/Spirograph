84d7e264491098015d7a91c7022db009dde9eead
Christopher Canaday <chrislcanaday@gmail.com>
2022-09-23 17:17:25 -0400

    in progress halfway makes spirographs

--- include/main.h
+++ include/main.h
@@ -1,22 +1,46 @@
 #ifndef SPIROGRAPH_INCLUDE_MAIN_H
 #define SPIROGRAPH_INCLUDE_MAIN_H
 
-struct Spirograph{
-    double theta; // angle
-    double r_i; // inner circle radius
-    double r_o; // outer circle radius
-    double c_i_dist; // distance from edge of inside circle
-    double x;
-    double y;
+#include <string>
+
+struct color{
+    int r;
+    int g;
+    int b;
 };
 
-void setDefaults(Spirograph &s){
-    s.theta = 0;
-    s.c_i_dist = 0;
-    s.x = 0;
-    s.y = 0;
-    s.r_i = 0;
-    s.r_o = 0;
+struct Spirograph{
+    int r_i; // inner circle radius
+    double r_o; // outer circle radius
+    double x; // x position
+    double y; // y position
+    double rho; // distance from center of inner circle (the point)
+    double l; // rho / r_i  represents how far some point A is from the center of inner circle
+    double k; // r_i / r_o  represents size of inner circle with respect to outer circle
+    color colors;
+    int rotations;
+    int outerteethcount;
+    int innerteethcount;
+
+    void setDefaults();
+    void getInput(int arc, char **argv);
+    void draw();
+    void createSpiroGraph();
+};
+
+void Spirograph::setDefaults(){
+    x = 0;
+    y = 0;
+    r_i = 97;
+    r_o = 200;
+    rho = 70;
+    l = rho / r_i;
+    k = r_i / r_o;
+    colors.r = 0;
+    colors.g = 0;
+    colors.b = 0;
+    outerteethcount = 20;
+    innerteethcount = 5;
 }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,70 @@
 #include <cstdio>
 #include <iostream>
+#include <sstream>
 #include <cmath>
+#include <algorithm>
 
 #include "../include/main.h"
+using namespace std;
+
+void printUsage();
+void printPrompt();
 
 int main(int argc, char **argv){
     Spirograph graph;
 
-    setDefaults(graph);
-    
+    graph.setDefaults();
+    graph.getInput(argc, argv);
+
+    //graph.createSpiroGraph();
+    //graph.draw();
+
     return 0;
+}
+
+void Spirograph::getInput(int arc, char **argv){
+    string s;
+    int gcd;
+
+    while(getline(cin,s)){
+        stringstream ss(s);
+
+        ss >> colors.r >> colors.g >> colors.b >> outerteethcount >> innerteethcount >> rho;
+        //gcd();
+        gcd = __gcd(outerteethcount,innerteethcount);
+        r_o = outerteethcount / gcd;
+        r_i = outerteethcount / gcd;
+        createSpiroGraph();
+    }
+}
+
+void Spirograph::draw(){
+    int i,n;
+    double t,it,step;
+    
+    n = 2000 * r_o;
+
+    //n = 2000;
+    it = r_o * 3.1459 * 2 / n;
+
+    printf("newline color %d %d %d\n",colors.r,colors.g,colors.b);
+    printf("pts\n");
+
+    t = 0.0;
+    for(i = 0; i < n; i++){
+        x = r_o*((1-k)*cos(t) + l*k*cos(((1-k)/k) * t));
+        y = r_o*((1-k)*sin(t) - l*k*sin(((1-k)/k) * t));
+        printf("%.3lf %.3lf\n",x,y);
+        t += it;
+    }
+}
+
+void Spirograph::createSpiroGraph(){
+    printf("newgraph\n");
+    //printf("xaxis min %lf max %lf nodraw\n", -1*(r_o+1),r_o+1);
+    //printf("yaxis min %lf max %lf nodraw\n", -1*(r_o+1),r_o+1);
+    printf("xaxis nodraw\n");
+    printf("yaxis nodraw\n");
+
+    draw();
 }
