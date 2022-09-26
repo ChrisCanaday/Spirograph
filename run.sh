96e984d5015a30534bf5e1d65ae7fcc98a35fd2d
Christopher Canaday <chrislcanaday@gmail.com>
2022-09-26 19:16:55 -0400

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,16 @@
-./bin/Spiro < input.txt > test.txt
-./jgraph/jgraph -P test.txt | ps2pdf - | convert -density 300 - -quality 100 test.jpg
+#!/bin/bash
+
+# This script runs the Spiro program with the correct files.
+
+
+if [ "$#" -lt 2 ]; then
+    echo "usage: run.sh <input-file> <output-file>"
+    exit
+fi
+
+IN=$1
+OUT=$2
+
+./bin/Spiro < $IN > tmp.txt
+./jgraph/jgraph -P tmp.txt | ps2pdf - | convert -density 300 - -quality 100 $OUT
+rm tmp.txt
